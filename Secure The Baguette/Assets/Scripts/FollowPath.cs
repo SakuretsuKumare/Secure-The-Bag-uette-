e8d17ff6917d647c410d9206f35d1e677e7c7dac
David Rozell <Drozell@student.fitchburgstate.edu>
2022-01-29 12:31:44 -0500

    Basic enemy path AI and other essentials

---
+++ Secure The Baguette/Assets/Scripts/FollowPath.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    // put the points from unity interface
+    public Transform[] wayPointList;
+
+    public int currentWayPoint = 0;
+    Transform targetWayPoint;
+
+    public float speed = 4f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if we have somewere to walk
+        if (currentWayPoint < this.wayPointList.Length)
+        {
+            if (targetWayPoint == null)
+                targetWayPoint = wayPointList[currentWayPoint];
+            walk();
+        }
+    }
+
+    void walk()
+    {
+        // rotate towards the target
+        transform.forward = Vector3.RotateTowards(transform.forward, targetWayPoint.position - transform.position, speed * Time.deltaTime, 0.0f);
+
+        // move towards the target
+        transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position, speed * Time.deltaTime);
+
+        if (transform.position == targetWayPoint.position)
+        {
+            if (currentWayPoint != this.wayPointList.Length - 1)
+            {
+                currentWayPoint++;
+            }
+            else
+            {
+                currentWayPoint = 0;
+            }
+            
+            targetWayPoint = wayPointList[currentWayPoint];
+        }
+    }
+}
