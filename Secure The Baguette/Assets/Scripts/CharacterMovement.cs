cef1fa97d8db26e88910f47fa0afa29175a71f87
David Wu <dawncoming3@gmail.com>
2022-02-07 17:24:42 -0500

    Character Sprinting & Crouching
    Left Shift to sprint
    Left CRTL to crouch.
    Added to cursor script.

--- Secure The Baguette/Assets/Scripts/CharacterMovement.cs
+++ Secure The Baguette/Assets/Scripts/CharacterMovement.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // This code was used from Brackeys https://www.youtube.com/watch?v=4HpC--2iowE
+// Code was also used from ZeveonHD https://www.youtube.com/watch?v=D2LqcIxAQpY&t=65s
 
 public class CharacterMovement : MonoBehaviour
 {
+    // Declaring and initiating variables.
+
     public CharacterController controller;
     public Transform cam;
     public Transform groundCheck;
@@ -16,6 +19,10 @@ public class CharacterMovement : MonoBehaviour
     private float groundDistance = 0.2f;
     public float jumpHeight = 3f;
     float turnSmoothVelocity;
+    public bool isSprinting = false;
+    public bool isCrouching = false;
+    public float sprintingMultiplier = 1.5f;
+    public float crouchingMultiplier = 0.5f;
     Vector3 velocity;
     bool isGrounded;
 
@@ -28,6 +35,7 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Checks if the player is touching the ground or not.
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
@@ -35,6 +43,7 @@ public class CharacterMovement : MonoBehaviour
             velocity.y = -2f;
         }
 
+        // Getting user input for movement.
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -46,12 +55,57 @@ public class CharacterMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+
+            // Regular walking movespeed, no crouching or sprinting.
+            if ((isSprinting == false) && (isCrouching == false))
+            {
+                controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            }
+
+            // The character is sprinting
+            else if ((isSprinting == true) && (isCrouching == false))
+            {
+                controller.Move(moveDirection.normalized * speed * sprintingMultiplier * Time.deltaTime);
+            }
+
+            // The character is crouching.
+            else if ((isSprinting == false) && (isCrouching == true))
+            {
+                controller.Move(moveDirection.normalized * speed * crouchingMultiplier * Time.deltaTime);
+            }
+
+            else
+            {
+                controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            }
         }
 
+        // The player can jump with the space bar.
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Checks if the player is sprinting or not (Left Shift to sprint)
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+        }
+
+        else
+        {
+            isSprinting = false;
+        }
+
+        // Checks if the player is crouching or not (Left CTRL to crouch)
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            isCrouching = true;
+        }
+
+        else
+        {
+            isCrouching = false;
         }
 
         velocity.y += gravity * Time.deltaTime;
--- Secure The Baguette/Assets/Scripts/CursorScript.cs
+++ Secure The Baguette/Assets/Scripts/CursorScript.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cursor : MonoBehaviour
+public class CursorScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        HideCursor();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ShowCursor()
+    {
+        Cursor.visible = true;
+    }
+
+    public void HideCursor()
+    {
+        Cursor.visible = false;
     }
 }
